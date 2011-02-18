54ab908dc9c53c4a350d2fb58572bc0d4fe1d334
tily <tidnlyam@gmail.com>
2011-02-19 02:56:19 +0900

    modify BlockNoise breaker

--- src/cc/omora/android/brokencamera/breakers/BlockNoise.java
+++ src/cc/omora/android/brokencamera/breakers/BlockNoise.java
@@ -7,7 +7,7 @@ import java.io.ByteArrayOutputStream;
 public class BlockNoise extends Breaker {
         public Bitmap breakData(Bitmap bitmap, int level) {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.JPEG, 101-level, bos);
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100-level, bos);
 		byte[] data = bos.toByteArray();
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		return BitmapFactory.decodeByteArray(data, 0, data.length, options).copy(Bitmap.Config.RGB_565, true);
